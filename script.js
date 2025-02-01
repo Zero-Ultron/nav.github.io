a8b5aca0fea046e26b4b05ea5feb86ccd1cb646a
Aditya <adityadwic250808@gmail.com>
2025-02-01 14:31:35 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+     document.getElementById('toggle-button').addEventListener('click', function() {
+    const menu = document.getElementById('nav-mobile');
+    menu.classList.toggle('mobile');
+
+});
